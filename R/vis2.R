d32a03e2ad1ac83ec9cd0c2c10632e93332cc718
Michal Bojanowski <michal2992@gmail.com>
2016-06-14 00:08:08 +0200

    Adding make_net2

--- R/vis2.R
+++ R/vis2.R
@@ -12,25 +12,60 @@ get_data2 <- function(file, sheet=NULL, ...) {
   wb <- XLConnect::loadWorkbook(filename=file)
   sheet_names <- XLConnect::getSheets(wb)
   sh <- grep("^DC|^WF", sheet_names)
+  # What to get
   if( is.null(sheet) ) {
     toget <- sh
   } else {
-    if( is.numeric(sheet) ) {
-      stopifnot( all(sheet %in% seq(along=sh) ) )
-      toget <- sheet
-    } else {
-      stopifnot(is.character(sheet))
-      stopifnot(all(sheet %in% sheet_names))
-      toget <- which( sheet_names %in% sheet)
-    }
+    stopifnot(is.character(sheet))
+    stopifnot(all(sheet %in% sheet_names))
+    toget <- which(sheet_names %in% sheet)
   }
   # Get
   if( length(toget) == 1) {
     rval <- XLConnect::readWorksheet(wb, sheet=toget, ...)
+    attr(rval, "idi_id") <- sheet_names[toget]
     return(rval)
   } else {
-    rval <- lapply(toget, function(i) XLConnect::readWorksheet(wb, sheet=i, ...))
+    rval <- lapply(toget, function(i)
+      structure(
+        XLConnect::readWorksheet(wb, sheet=i, ...),
+        idi_id = sheet_names[i]
+      ) )
     names(rval) <- sheet_names[toget]
     return(rval)
   }
+}
+
+
+
+#' Create network object
+#'
+#' @param d data frame
+#'
+#' @return igraph object
+#' @export
+make_net2 <- function(d) {
+  # Vertex DB
+  vdb <- data.frame(
+    id=d$Nr.wezła.2,
+    grupa = gsub("[^0-9,]", "", d$Przynależność.do.grup..na.podstawie.zdjęcie..grupa.może.być.też.dwuosobowa.)
+  )
+  # Edge DB
+  edb <- rbind(
+    data.frame(
+      ego = d$Nr.wezła.1,
+      alter = d$Nr.wezła.2,
+      res = gsub("[^0-9,]", "", d$Kategoria.zasobów.pzekazanych)
+    ),
+    data.frame(
+      ego = d$Nr.wezła.2,
+      alter = d$Nr.wezła.1,
+      res = gsub("[^0-9,]", "", d$Kategorie.zasobów.otrzymanych)
+    )
+  )
+  edb <- subset(edb, ego != alter)
+  
+  g <- igraph::graph_from_data_frame(edb, directed = TRUE, vertices = vdb)
+  g$idi_id <- attr(d, "idi_id")
+  g
 }
--- tests/testthat/test-vis2.R
+++ tests/testthat/test-vis2.R
@@ -7,3 +7,13 @@ test_that("get_data2 gets all sheets", {
   d <- get_data2(xlsx)
   expect_true( is.list(d))
 })
+
+
+
+
+context("Testing make_net2")
+
+test_that("make_data2 works at all", {
+  d <- get_data2(xlsx)
+  g <- make_net2(d)
+})
